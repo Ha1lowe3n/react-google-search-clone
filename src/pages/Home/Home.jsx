cb6f52269e506e168960266f8d83748fbafb535e
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-01-17 04:32:19 +0800

    init Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './Home.scss';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Hompage</h2>
+
+      <div className="home__header">
+        <div className="home__header-left">
+          {/* link */}
+          {/* link */}
+        </div>
+        <div className="home__header-right">
+          {/* link */}
+          {/* link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+
+      <div className="home__body"></div>
+    </div>
+  );
+}
+
+export default Home;
